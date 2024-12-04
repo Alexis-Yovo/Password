9e5ed7f9e4e3cd5f3bd36681e87b19f1b53209cb
Alexis-Yovo <74179398+Alexis-Yovo@users.noreply.github.com>
2024-12-04 03:19:43 +0100

    Add files via upload

---
+++ Ex 4 accès contrôlé.py
@@ -0,0 +1,10 @@
+login=input("Entrez votre login : ")
+password=input("Entrez votre mot de passe : ")
+if login == "Alexis" and password == "bb123":
+    print("Bienvenue!")
+else:
+    print("Utilisateur non reconnu,au revoir!")
+if login =="Alexis" and password == "azerty":
+    print("Mot de passe incorrect")
+
+
